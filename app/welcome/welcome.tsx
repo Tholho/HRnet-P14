d26f2030b614b983140395c321ce2033c704011c
Nick Pseudo <tomdop.dakar@gmail.com>
2025-02-26 10:44:55 +0100

    state selector ok

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,15 +3,15 @@ import logo from "./logoHRnet.png"
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+    <main className="flex items-center justify-center pt-8 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-8 min-h-0">
         <header className="flex flex-col items-center gap-9">
-            <img
-              src={logo}
-              alt="Wealth Health"
-            />
+          <img
+            src={logo}
+            alt="Wealth Health"
+          />
         </header>
-        <NavLink to="/employees-list">View Current Employees</NavLink>
+        <NavLink to="/employees-list" className="rounded-2xl border p-3 bg-gray-400 hover:bg-gray-300 border-gray-200 dark:border-gray-400" >View Current Employees</NavLink>
         <section className="max-w-[400px] space-y-6 px-4">
           <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
@@ -25,11 +25,19 @@ export function Welcome() {
                 </div>
               ))}
               <fieldset className="rounded-3xl border border-gray-200 dark:border-gray-700">
-                <legend>Address</legend>
+                <legend className="ml-5">Address</legend>
                 {addressFields.map(({ id, text, type, isSelect = false }) => (
                   <div className="m-5 flex flex-col w-50" key={id}>
                     <label htmlFor={id}>{text}</label>
-                    {isSelect ? <select name={id} id={id}></select>
+                    {isSelect ? <select className="p-2 bg-white text-black" name={id} id={id}>
+                      {
+                        statesList.map((state) => (
+                          <option key={state.abbreviation} value={state.abbreviation}>
+                            {state.name}
+                          </option>
+                        ))
+                      }
+                    </select>
                       :
                       <input type={type} id={id} className="bg-white text-black p-1"></input>
                     }
@@ -43,7 +51,6 @@ export function Welcome() {
     </main>
   );
 }
-
 
 const employeeFields = [
   {
@@ -91,44 +98,242 @@ const addressFields = [
   },
 ];
 
-const resources = [
+const statesList = [
   {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
+    "name": "Alabama",
+    "abbreviation": "AL"
   },
   {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
+    "name": "Alaska",
+    "abbreviation": "AK"
   },
+  {
+    "name": "American Samoa",
+    "abbreviation": "AS"
+  },
+  {
+    "name": "Arizona",
+    "abbreviation": "AZ"
+  },
+  {
+    "name": "Arkansas",
+    "abbreviation": "AR"
+  },
+  {
+    "name": "California",
+    "abbreviation": "CA"
+  },
+  {
+    "name": "Colorado",
+    "abbreviation": "CO"
+  },
+  {
+    "name": "Connecticut",
+    "abbreviation": "CT"
+  },
+  {
+    "name": "Delaware",
+    "abbreviation": "DE"
+  },
+  {
+    "name": "District Of Columbia",
+    "abbreviation": "DC"
+  },
+  {
+    "name": "Federated States Of Micronesia",
+    "abbreviation": "FM"
+  },
+  {
+    "name": "Florida",
+    "abbreviation": "FL"
+  },
+  {
+    "name": "Georgia",
+    "abbreviation": "GA"
+  },
+  {
+    "name": "Guam",
+    "abbreviation": "GU"
+  },
+  {
+    "name": "Hawaii",
+    "abbreviation": "HI"
+  },
+  {
+    "name": "Idaho",
+    "abbreviation": "ID"
+  },
+  {
+    "name": "Illinois",
+    "abbreviation": "IL"
+  },
+  {
+    "name": "Indiana",
+    "abbreviation": "IN"
+  },
+  {
+    "name": "Iowa",
+    "abbreviation": "IA"
+  },
+  {
+    "name": "Kansas",
+    "abbreviation": "KS"
+  },
+  {
+    "name": "Kentucky",
+    "abbreviation": "KY"
+  },
+  {
+    "name": "Louisiana",
+    "abbreviation": "LA"
+  },
+  {
+    "name": "Maine",
+    "abbreviation": "ME"
+  },
+  {
+    "name": "Marshall Islands",
+    "abbreviation": "MH"
+  },
+  {
+    "name": "Maryland",
+    "abbreviation": "MD"
+  },
+  {
+    "name": "Massachusetts",
+    "abbreviation": "MA"
+  },
+  {
+    "name": "Michigan",
+    "abbreviation": "MI"
+  },
+  {
+    "name": "Minnesota",
+    "abbreviation": "MN"
+  },
+  {
+    "name": "Mississippi",
+    "abbreviation": "MS"
+  },
+  {
+    "name": "Missouri",
+    "abbreviation": "MO"
+  },
+  {
+    "name": "Montana",
+    "abbreviation": "MT"
+  },
+  {
+    "name": "Nebraska",
+    "abbreviation": "NE"
+  },
+  {
+    "name": "Nevada",
+    "abbreviation": "NV"
+  },
+  {
+    "name": "New Hampshire",
+    "abbreviation": "NH"
+  },
+  {
+    "name": "New Jersey",
+    "abbreviation": "NJ"
+  },
+  {
+    "name": "New Mexico",
+    "abbreviation": "NM"
+  },
+  {
+    "name": "New York",
+    "abbreviation": "NY"
+  },
+  {
+    "name": "North Carolina",
+    "abbreviation": "NC"
+  },
+  {
+    "name": "North Dakota",
+    "abbreviation": "ND"
+  },
+  {
+    "name": "Northern Mariana Islands",
+    "abbreviation": "MP"
+  },
+  {
+    "name": "Ohio",
+    "abbreviation": "OH"
+  },
+  {
+    "name": "Oklahoma",
+    "abbreviation": "OK"
+  },
+  {
+    "name": "Oregon",
+    "abbreviation": "OR"
+  },
+  {
+    "name": "Palau",
+    "abbreviation": "PW"
+  },
+  {
+    "name": "Pennsylvania",
+    "abbreviation": "PA"
+  },
+  {
+    "name": "Puerto Rico",
+    "abbreviation": "PR"
+  },
+  {
+    "name": "Rhode Island",
+    "abbreviation": "RI"
+  },
+  {
+    "name": "South Carolina",
+    "abbreviation": "SC"
+  },
+  {
+    "name": "South Dakota",
+    "abbreviation": "SD"
+  },
+  {
+    "name": "Tennessee",
+    "abbreviation": "TN"
+  },
+  {
+    "name": "Texas",
+    "abbreviation": "TX"
+  },
+  {
+    "name": "Utah",
+    "abbreviation": "UT"
+  },
+  {
+    "name": "Vermont",
+    "abbreviation": "VT"
+  },
+  {
+    "name": "Virgin Islands",
+    "abbreviation": "VI"
+  },
+  {
+    "name": "Virginia",
+    "abbreviation": "VA"
+  },
+  {
+    "name": "Washington",
+    "abbreviation": "WA"
+  },
+  {
+    "name": "West Virginia",
+    "abbreviation": "WV"
+  },
+  {
+    "name": "Wisconsin",
+    "abbreviation": "WI"
+  },
+  {
+    "name": "Wyoming",
+    "abbreviation": "WY"
+  }
 ];
+
